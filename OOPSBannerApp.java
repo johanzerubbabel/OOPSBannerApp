e369d25d4bb2d0d11b92a378d23dece430463f02
johanzerubbabel <johanjm0305@gmail.com>
2026-02-16 23:47:59 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -31,17 +31,15 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String banner = String.join("\n",
-            "   ***       ***     ******         *****  ",
-            " **   **   **   **   **    **     **     * ",
-            "**     ** **     **  **      ** **       ",
-            "**     ** **     **  **    **     **  ",
-            "**     ** **     **  ******         *** ",
-            "**     ** **     **  **                 **  ",
-            "**     ** **     **  **                  **",
-            " **   **   **   **   **                 ** ",
-            "   ***       ***     **            *****"
-        );
-
-        System.out.println(banner);
+        System.out.println(String.join(" ","   ***    ","   ***    "," ******    ","     *****  "));
+        System.out.println(String.join(" "," **    ** "," **   **  "," **    **  ","   **     * "));
+        System.out.println(String.join(" ","**     ** ","**     ** "," **      **"," **       "));
+        System.out.println(String.join(" ","**     ** ","**     ** "," **    **  ","   **  "));
+        System.out.println(String.join(" ","**     ** ","**     ** "," ******    ","     *** "));
+        System.out.println(String.join(" ","**     ** ","**     ** "," **        ","         **  "));
+        System.out.println(String.join(" ","**     ** ","**     ** "," **        ","          **"));
+        System.out.println(String.join(" "," **   **  "," **   **  "," **        ","         ** "));
+        System.out.println(String.join(" ","   ***    ","   ***    "," **        ","    *****"));
+        
     }
 }
