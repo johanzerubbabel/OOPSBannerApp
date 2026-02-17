e9de2237c40c4e17cf5f893dfa9c9a25ca7681a1
johanzerubbabel <johanjm0305@gmail.com>
2026-02-17 10:20:42 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,3 @@
-/**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application
- * that displays the object oriented programming
- * system OOPS banner to the console.
- *
- * @author Johan
- * @version 1.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
